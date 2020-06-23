903435627346dce2e19509420d72fd0679472a69
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2020-06-22 23:40:27 -0300

    refactor character.js

--- scripts/game/character.js
+++ scripts/game/character.js
@@ -1,29 +1,21 @@
+const CHARACTER_HEIGHT = 135;
+const CHARACTER_WIDTH = 110;
+const IMAGE_HEIGHT = 270;
+const IMAGE_WIDTH = 220;
+const IMAGE_FRAMES = 16;
+const IMAGE_FRAMES_ROWS = 4;
+const IMAGE_FRAMES_COLUMNS = 4;
+
 class Character {
   constructor(image){
     this.image = image;
-    this.matrix = [
-      [0, 0],
-      [220, 0],
-      [440, 0],
-      [660, 0],
-      [0, 270],
-      [220, 270],
-      [440, 270],
-      [660, 270],
-      [0, 540],
-      [220, 540],
-      [440, 540],
-      [660, 540],
-      [0, 810],
-      [220, 810],
-      [440, 810],
-      [660, 810],
-    ];
+    this.matrix = this.calculateMatrix();
+    this.coordinates = {x: 0, y: height - CHARACTER_HEIGHT}
     this.currentFrame = 0;
   }
   
   show(){
-      image(this.image, 0, height-135, 110, 135, this.matrix[this.currentFrame][0], this.matrix[this.currentFrame][1], 220, 270); // image, coordenadas da posição inicial (x, y), width, height, coordenadas interna da image, width e height interno da image
+    image(this.image, this.coordinates.x, this.coordinates.y, CHARACTER_WIDTH, CHARACTER_WIDTH, this.matrix[this.currentFrame][0], this.matrix[this.currentFrame][1], IMAGE_WIDTH, IMAGE_HEIGHT); 
     this.animate();
   }
   
@@ -33,6 +25,16 @@ class Character {
     if(this.currentFrame >= this.matrix.length){
       this.currentFrame = 0;
     }
-  
+  }
+
+  calculateMatrix(){
+    let matrix = [];
+    for (let i = 0; i < IMAGE_FRAMES; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < 2; j++) {
+            matrix[i][j] = j % 2 === 0 ? (i % 4) * IMAGE_WIDTH : parseInt(i/4) * IMAGE_HEIGHT;
+        }
+    }
+    return matrix;
   }
 }
